8d33ef0883dbf0c485c5157e3a577dc6298ee007
Ashish Prasad <77892788+Ashish558@users.noreply.github.com>
2022-03-12 22:30:13 +0530

    Add files via upload

---
+++ client/public/src/components/FormTabs/froms/ApplicationForm.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { Box, Typography } from '@mui/material'
+import { useSelector } from 'react-redux'
+
+import InputField from './inputField'
+import FormPagination from '../formPagination'
+import { updateAmount, updateInterest, updateTenure } from '../../../app/slices/loanForm'
+
+export default function ApplicationForm({ value, setValue }) {
+
+    const { amount, interest, tenure } = useSelector(state => state.loanForm)
+    const { errorMsg, successMsg } = useSelector(state => state.message)
+    
+    return (
+        <Box>
+            <Box mb='20px' >
+                <Typography variant='h5' fontWeight='600' mb='20px' >
+                    Loan Details
+                </Typography>
+                <Typography color='#65cf87' textAlign='center' mb='17px' >
+                    {errorMsg} {successMsg}
+                </Typography>
+                <InputField type='number' label='Loan Amount' value={amount} setValue={updateAmount} />
+                <InputField type='number' label='Interest' value={interest} setValue={updateInterest} />
+                <InputField type='number' label='Period of Loan' value={tenure} setValue={updateTenure} />
+
+            </Box>
+            <FormPagination setValue={setValue} value={value}  />
+
+        </Box>
+    )
+} 
