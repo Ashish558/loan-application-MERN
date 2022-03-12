29f2bdee801a9d4c8f8497fff5c64d0c8911b91c
Ashish Prasad <77892788+Ashish558@users.noreply.github.com>
2022-03-12 22:36:02 +0530

    Update constants.js

--- client/src/services/constants.js
+++ client/src/services/constants.js
@@ -1,4 +1,3 @@
 
-// export const domain = 'https://desolate-mesa-23396.herokuapp.com'
-export const domain = 'http://localhost:4000'
+export const domain = 'https://loan-application-5678.herokuapp.com/'
 
