d255a38261fda59d1bc5622b10f90b2e37f746d2
Ashish Prasad <77892788+Ashish558@users.noreply.github.com>
2022-03-12 23:08:01 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import FormTabs from '../components/FormTabs/fromTabs';
+import FormTabs from '../components/FormTabs/formTabs';
 
 
 
